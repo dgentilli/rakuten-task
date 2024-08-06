621472515e193e13f0774754b76d32c6b45288ca
Dave Gentilli <dgentilli@gmail.com>
2024-08-06 11:50:24 +0300

    Synchronize data fetching and sort/filter properly

--- app/Question1.tsx
+++ app/Question1.tsx
@@ -1,7 +1,8 @@
 import ItemGrid from '@/components/ListItems/ItemGrid';
 import ItemRow from '@/components/ListItems/ItemRow';
+import useFetchUsers from '@/hooks/useFetchUsers';
 import { User } from '@/types/User';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import {
   View,
   Text,
@@ -12,16 +13,19 @@ import {
   SafeAreaView,
   StyleProp,
   ViewStyle,
+  ActivityIndicator,
 } from 'react-native';
 
 const Question1 = () => {
-  const mockData = require('../assets/MOCK_DATA.json');
   const [layout, setLayout] = useState<'list' | 'grid'>('list');
-  const [data, setData] = useState(mockData);
+  const { data: apiData, isLoading, error, fetchPageData } = useFetchUsers();
+  const [data, setData] = useState<User[]>(apiData);
+  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc');
+  const [showOnlyLargeAvatar, setShowOnlyLargeAvatar] = useState(false);
 
-  const onPressItem = () => {
+  const onPressItem = useCallback(() => {
     console.log('open item');
-  };
+  }, []);
 
   const getNumColumns = useCallback(() => {
     return layout === 'list' ? 1 : 2;
@@ -31,53 +35,66 @@ const Question1 = () => {
     return layout === 'grid' ? { alignItems: 'center' } : null;
   }, [layout]);
 
-  const handleMissingLastName = (a: User, b: User) => {
+  const handleMissingLastName = useCallback((a: User, b: User) => {
     // If there is no last name, move the user to the end of the list.
     if (!a.last_name && !b.last_name) return 0; // Both missing, equal
     if (!a.last_name) return 1; // a is missing last name, so it comes after b
     if (!b.last_name) return -1; // b is missing last name, so it comes after a
     return undefined;
-  };
+  }, []);
 
-  const compareLastName = (a: User, b: User, order: 'asc' | 'desc') => {
-    const missingHandlingResult = handleMissingLastName(a, b);
-    if (missingHandlingResult !== undefined) return missingHandlingResult;
+  const compareLastName = useCallback(
+    (a: User, b: User, order: 'asc' | 'desc') => {
+      const missingHandlingResult = handleMissingLastName(a, b);
+      if (missingHandlingResult !== undefined) return missingHandlingResult;
 
-    const aLastName = a.last_name ?? '';
-    const bLastName = b.last_name ?? '';
+      const aLastName = a.last_name ?? '';
+      const bLastName = b.last_name ?? '';
 
-    if (order === 'asc') {
-      if (aLastName < bLastName) return -1;
-      if (aLastName > bLastName) return 1;
-    } else if (order === 'desc') {
-      if (aLastName < bLastName) return 1;
-      if (aLastName > bLastName) return -1;
-    }
+      if (order === 'asc') {
+        if (aLastName < bLastName) return -1;
+        if (aLastName > bLastName) return 1;
+      } else if (order === 'desc') {
+        if (aLastName < bLastName) return 1;
+        if (aLastName > bLastName) return -1;
+      }
 
-    return 0;
-  };
+      return 0;
+    },
+    []
+  );
 
   const onPressSortAscending = useCallback(() => {
-    let tempData = mockData.map((user: User) => ({ ...user }));
-    tempData.sort((a: User, b: User) => compareLastName(a, b, 'asc'));
-    setData(tempData);
-  }, [mockData]);
+    setSortOrder('asc');
+  }, [sortOrder]);
 
   const onPressSortDescending = useCallback(() => {
-    let tempData = mockData.map((user: User) => ({ ...user }));
-    tempData.sort((a: User, b: User) => compareLastName(a, b, 'desc'));
-    setData(tempData);
-  }, [mockData]);
+    setSortOrder('desc');
+  }, [sortOrder]);
 
   const onPressFilterByAvatarLarge = useCallback(() => {
-    const filteredData = data.filter((user: User) =>
-      Boolean(user.avatar_large)
-    );
-    setData(filteredData);
-  }, [data]);
+    setShowOnlyLargeAvatar(!showOnlyLargeAvatar);
+  }, [showOnlyLargeAvatar]);
+
+  const applyFilterAndSorting = useCallback(() => {
+    let tempData = [...apiData];
+
+    if (showOnlyLargeAvatar) {
+      tempData = tempData.filter((user) => Boolean(user.avatar_large));
+    }
+
+    tempData.sort((a, b) => compareLastName(a, b, sortOrder));
+
+    setData(tempData);
+  }, [apiData, showOnlyLargeAvatar, sortOrder]);
+
+  useEffect(() => {
+    applyFilterAndSorting();
+  }, [apiData, showOnlyLargeAvatar, sortOrder]);
 
   const renderItem = ({ item }: { item: User }) => {
     const { id } = item;
+
     return (
       <TouchableOpacity key={id} onPress={onPressItem}>
         {layout === 'list' ? <ItemRow item={item} /> : <ItemGrid item={item} />}
@@ -138,6 +155,9 @@ const Question1 = () => {
           renderItem={renderItem}
           numColumns={getNumColumns()}
           key={layout}
+          onEndReached={fetchPageData}
+          ListFooterComponent={isLoading ? <ActivityIndicator /> : null}
+          ListEmptyComponent={<Text>No Items Available...</Text>}
         />
       </View>
     </SafeAreaView>
--- hooks/useFetchUsers.ts
+++ hooks/useFetchUsers.ts
@@ -4,20 +4,23 @@ import { useState, useEffect, useCallback } from 'react';
 const useFetchUsers = () => {
   const [data, setData] = useState<User[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isFetchingData, setIsFetchingData] = useState(false);
   const [error, setError] = useState('');
   const [pageUrls, setPageUrls] = useState<string[]>([]);
   const [currentPage, setCurrentPage] = useState(0);
-  const [hasMore, setHasMore] = useState(true);
 
   const fetchPageUrls = useCallback(async () => {
+    // Responsible for: fetching the urls and returning the array of urls
+    // chose to return the urls instead of setting state here to keep things in sync
     try {
       setIsLoading(true);
       const response = await fetch(
         'https://gist.githubusercontent.com/dsandin/c8ed6c5a9486f311f4725f221b912220/raw/8c6d2d8e1f339d02e0ff3d2990560a4862c4beae/users_page_list'
       );
       const { pages } = await response.json();
-      setPageUrls(pages);
-    } catch (err) {
+      return pages;
+    } catch (error) {
+      console.error(error);
       setError('Something went wrong.');
     } finally {
       setIsLoading(false);
@@ -25,38 +28,47 @@ const useFetchUsers = () => {
   }, []);
 
   const fetchPageData = useCallback(async () => {
+    // Responsible for: Fetching the page data. Checks to make sure we aren't already loading
+    // and makes sure we don't try to load a page that doesn't exist.
     if (currentPage >= pageUrls.length) return;
-
-    setIsLoading(true);
+    if (isFetchingData) return;
 
     try {
+      setIsFetchingData(true);
       const response = await fetch(pageUrls[currentPage]);
       const pageData = await response.json();
+
       setData((prevData) => [...prevData, ...pageData]);
       setCurrentPage((prevPage) => prevPage + 1);
-      //   setHasMore(currentPage + 1 < pageUrls.length);
-    } catch (err) {
+    } catch (error) {
+      console.error(error);
       setError('Something went wrong.');
     } finally {
-      setIsLoading(false);
+      setIsFetchingData(false);
     }
-  }, [pageUrls, currentPage]);
+  }, [pageUrls, currentPage, isFetchingData]);
 
-  useEffect(() => {
-    fetchPageUrls();
+  const fetchFirstPage = useCallback(async () => {
+    // Responsible for: calling fetchPageUrls, keeping pageUrls in sync, calling fetchPageData
+    const urls = await fetchPageUrls();
+
+    // Check that we have data and not an error
+    if (Array.isArray(urls) && urls.length > 0) {
+      setPageUrls(() => urls);
+      fetchPageData();
+    } else {
+      setError('Something went wrong.');
+    }
   }, []);
 
   useEffect(() => {
-    if (pageUrls.length > 0) {
-      fetchPageData();
-    }
-  }, [pageUrls]);
+    fetchFirstPage();
+  }, []);
 
   return {
     data,
     isLoading,
     error,
-    hasMore,
     fetchPageData,
   };
 };
